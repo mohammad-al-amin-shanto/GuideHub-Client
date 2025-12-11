fe18968fe8c3555e1249f20eb7aa431f4058f926
alamin-shanto <mdalaminshanto1993@gmail.com>
2025-12-12 00:21:57 +0600

    added home page

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+import HeroSearch from "../components/HeroSearch";
+import FeaturedCities from "../components/FeaturedCities";
+import TopGuides from "../components/TopGuides";
+import HowItWorks from "../components/HowItWorks";
+import Categories from "../components/Categories";
+import Testimonials from "../components/Testimonials";
+
+export default function Home() {
+  return (
+    <div className="space-y-20">
+      <section id="hero" className="pt-12">
+        <HeroSearch />
+      </section>
+
+      <section id="how" className="container mx-auto px-4">
+        <HowItWorks />
+      </section>
+
+      <section id="featured" className="container mx-auto px-4">
+        <FeaturedCities />
+      </section>
+
+      <section id="categories" className="container mx-auto px-4">
+        <Categories />
+      </section>
+
+      <section id="top-guides" className="container mx-auto px-4">
+        <TopGuides />
+      </section>
+
+      <section id="testimonials" className="container mx-auto px-4 pb-12">
+        <Testimonials />
+      </section>
+    </div>
+  );
+}
